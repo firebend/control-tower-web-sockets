fbae51d495c137c3ef937a71b731a866d0f78874
Jacob Atchley <yaaqob.atchley@gmail.com>
2022-04-21 15:16:38 -0500

    scaffold angular

---
+++ ct-rte-ws/apps/ct-rte-ws-sample/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ct-rte-ws-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ct-rte-ws-sample';
+}
