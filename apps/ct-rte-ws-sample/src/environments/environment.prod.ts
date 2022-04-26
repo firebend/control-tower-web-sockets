442d8047699a099a23e936a01f97e876b7234b1b
Jacob Atchley <yaaqob.atchley@gmail.com>
2022-04-26 17:45:35 -0500

    only allow yarn

--- apps/ct-rte-ws-sample/src/environments/environment.prod.ts
+++ apps/ct-rte-ws-sample/src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
+import config from './auth-config';
+
+const { domain, clientId, audience, errorPath } = config as {
+  domain: string;
+  clientId: string;
+  audience?: string;
+  errorPath: string;
+};
+
 export const environment = {
   production: true,
+  auth: {
+    domain,
+    clientId,
+    audience,
+    redirectUri: window.location.origin,
+    errorPath
+  },
+  httpInterceptor: {
+    allowedList: [`*`],
+  },
 };
