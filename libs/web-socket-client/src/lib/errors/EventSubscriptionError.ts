25fb08a4acf31a96289e3237158a94f33e7b7dcf
Jacob Atchley <yaaqob.atchley@gmail.com>
2022-04-27 09:01:43 -0500

    error docs

--- libs/web-socket-client/src/lib/errors/EventSubscriptionError.ts
+++ libs/web-socket-client/src/lib/errors/EventSubscriptionError.ts
@@ -1,11 +1,21 @@
 import { IModelError } from '../models/ModelStateResult';
 
+/**
+ * Encapsulates data for an event subscription error.
+ */
 export class EventSubscriptionError extends Error {
 
+  /**
+   * Creates a new instance.
+   * @param errors The errors that ocurred during the event subscription.
+   */
   constructor(errors: IModelError[] = []) {
     super('An error occurred while subscribing to an event');
     this.errors = errors;
   }
 
+  /**
+   * The errors that ocurred during the event subscription.
+   */
   public readonly errors : IModelError[] = [];
 }
