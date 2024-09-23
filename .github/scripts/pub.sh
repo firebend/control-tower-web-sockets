37bda16e443ba8d75e3c96ead409f03169acf073
Jacob Atchley <yaaqob.atchley@gmail.com>
2024-09-23 13:20:18 -0500

    fix: publish logging

--- .github/scripts/pub.sh
+++ .github/scripts/pub.sh
@@ -15,11 +15,19 @@ PARENT_DIR="$PWD"
 ROOT_DIR="."
 echo "Removing Dist"
 rm -rf "${ROOT_DIR:?}/dist"
+
 COMMIT_MESSAGE="$(git log -1 --pretty=format:"%s")"
+echo "Commit Message: $COMMIT_MESSAGE"
+
 RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
+echo "Release Type: $RELEASE_TYPE"
+
 DRY_RUN=${DRY_RUN:-"False"}
+echo "Dry Run: $DRY_RUN"
 
 AFFECTED=$(node node_modules/.bin/nx affected:libs --plain)
+echo "Affected Libraries: $AFFECTED"
+
 if [ "$AFFECTED" != "" ]; then
   cd "$PARENT_DIR"
   echo "Copy Environment Files"
